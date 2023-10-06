4ad980c1186c1dd7a55d2df74067e14662e9e35e
robert-z <robertz.perez1985@gmail.com>
2023-10-06 13:12:56 +0200

    Generate static posts

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -1,5 +1,6 @@
 import MarkdownPreview from "@/components/markdownPreview";
 import getPost from "@/services/getPost";
+import getPosts from "@/services/getPosts";
 
 const PostPage = ({ params: { slug } }: { params: { slug: string } }) => {
   const post = getPost(slug);
@@ -16,5 +17,13 @@ const PostPage = ({ params: { slug } }: { params: { slug: string } }) => {
     </div>
   );
 };
+
+export async function generateStaticParams() {
+  const posts = getPosts();
+
+  return posts.map((post) => ({
+    slug: post.slug,
+  }));
+}
 
 export default PostPage;
